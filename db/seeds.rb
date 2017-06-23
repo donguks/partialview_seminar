6060df319a180075717367d2b6903def08595363
uk6342 <uk6342@gmail.com>
2017-06-24 01:22:21 +0900

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+50.times.each do
+  User.create(
+    name: Faker::Name.name,
+    age: Faker::Number.between(10, 40),
+    gender: ["male", "female"].sample,
+    food: Faker::Food.ingredient,
+    city: Faker::Address.city
+  )
+end
+
 for i in 1..10 do
   Post.create(
     content: Faker::Internet.email
